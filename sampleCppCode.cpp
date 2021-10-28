4b9e2363f49855474341cb8908c6a90f01878cf6
Shrish Tripathi <shrish0702@gmail.com>
2021-10-28 19:32:25 +0530

    Add sample cpp code file

---
+++ sampleCppCode.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    cout<<"Hey there...what's your name"<<endl;
+    string s;
+    cin>>s;
+    cout<<"Nice to e-meet you "+s+". Have a good day."<<endl;
+    return;
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
