a60d79b78ca7961d1a26013679b209d175d58b62
Shrish Tripathi <shrish0702@gmail.com>
2021-10-28 19:54:37 +0530

    Add sample c code file

---
+++ sampeCCode.c
@@ -0,0 +1,10 @@
+
+# include <stdio.h>
+int  main( )
+{
+char  name[100] ;
+printf("Hey there. What's your name?\n") ;
+scanf("%s",name) ;
+printf("Hello %s!! Glad to e-meet you.",name) ;
+return ( 0 );
+}
